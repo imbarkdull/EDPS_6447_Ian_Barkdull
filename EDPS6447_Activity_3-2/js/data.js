30ed546fd5595469dc2750bbfa5dfe7bac27a248
imbarkdull <59517813+imbarkdull@users.noreply.github.com>
2020-07-28 05:21:07 -0600

    Add files via upload

---
+++ EDPS6447_Activity_3-2/js/data.js
@@ -0,0 +1,64 @@
+var array_questions = [
+	{
+		statement: "How many Countries are there in the world?",
+		topic: "Geography",
+		sub_topic: "World Geography",
+		answer_key: "option3",
+		hint: "https://www.worldometers.info/geography/how-many-countries-are-there-in-the-world/",
+		type: "multiple_choice",
+		label: ["193", "172", "195", "230"],
+		video: { display: "false", source: "N/A" },
+		help: ["Not all countries are members of the United Nations."],
+	},
+	{
+		statement: "The Equator passes through the southern region of India?",
+		topic: "Geography",
+		sub_topic: "World Geography",
+		answer_key: "option2",
+		hint: "https://geoawesomeness.com/wp-content/uploads/2015/09/Equator-Geoawesomeness.jpg",
+		type: "true_false",
+		label: ["True", "False", "N/A", "N/A"],
+		video: { display: "false", source: "N/A" },
+		help: ["Trust your instincts."],
+	},
+	{
+		statement: "How many countries are located on the continent of South America?",
+		topic: "Geography",
+		sub_topic: "World Geography",
+		answer_key: "option4",
+		hint: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/Map_of_South_America.png/300px-Map_of_South_America.png",
+		type: "multiple_choice",
+		label: ["15","9","13","12"],
+		video: { display: "false", source: "N/A" },
+		help: ["Only looking for sovereign countries."],
+	},
+	{
+		statement: "What parallel establishes the border of United States and Canada?",
+		topic: "Geography",
+		sub_topic: "US Geography",
+		answer_key: "option2",
+		hint: "https://www.mapsofworld.com/lat_long/north-america.html",
+		type: "multiple_choice",
+		label: ["50th","49th","45th","51st"],
+		video: { display: "false", source: "N/A" },
+		help: ["Yes it is between 45th and 60th parallels."],
+	},
+	{
+		statement: "Los Angeles is farther west than Reno?",
+		topic: "Geography",
+		sub_topic: "US geography",
+		answer_key: "option2",
+		hint: "Reno sits on the Nevada/California border.",
+		type: "true_false",
+		label: ["True", "False", "N/A", "N/A"],
+		video: { display: "false", source: "N/A" },
+		help: ["No other help is availble at this time."],
+	},
+];
+
+var array_answers = [];
+
+console.log(array_questions);
+console.log(array_answers);
+console.log(array_questions[1].statement);
+
